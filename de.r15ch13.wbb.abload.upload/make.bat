be8dde879e49fb2c6bccdcd4a191b96797e9b1ba
r15ch13@gmx.de <r15ch13@gmx.de@ed87435c-ff07-11dd-9d1a-b59b2e1864b6>
2009-02-23 06:05:54 +0000

    wbb abload.de hack hinzugefügt

---
+++ de.r15ch13.wbb.abload.upload/make.bat
@@ -0,0 +1,19 @@
+@echo off
+
+for /f "tokens=2 delims=#n$" %%i in ('find "$n$" package.xml') do set var1=%%i
+for /f "tokens=2 delims=#v$" %%i in ('find "$v$" package.xml') do set var2=%%i
+
+del /f /q *.tar
+
+cd templates
+7z a -xr0@..\..\exclude.txt -ttar ..\templates.tar *
+
+cd ..\files
+7z a -xr0@..\..\exclude.txt -ttar ..\files.tar *
+
+cd ..
+7z a -xr0@..\exclude.txt -ttar "..\%var1%_v%var2%.tar" *
+
+
+del /f /q *.tar
+exit
