a5bce342b9305103fb08ec92d90609bde64c3e80
r15ch13@gmx.de <r15ch13@gmx.de@ed87435c-ff07-11dd-9d1a-b59b2e1864b6>
2009-02-26 18:38:32 +0000

    wgr: v1.1.0 + Klassenfarbe + Hintergrund-/Klassenfarbe deaktivierbar - Designänderung - Array überarbeitet

--- de.r15ch13.wbb.portal.box.wgr/files/lib/data/boxes/WGRBox.class.php
+++ de.r15ch13.wbb.portal.box.wgr/files/lib/data/boxes/WGRBox.class.php
@@ -3,27 +3,17 @@
 class WGRBox {
 	protected $wgrList = array();
 
-
-
 	public function __construct($data, $boxname = "") {
 		$this->wgrList['templatename'] = "wgrBox";
 		$this->getBoxStatus($data);
 		$this->wgrList['boxID'] = $data['boxID'];
+                $this->wgrList['debug'] = true;
 
-                $class = array('DR'=>'dr', 'WL'=>'wl', 'HU'=>'hu', 'WA'=>'wa', 'MA'=>'ma', 'PA'=>'pa', 'PR'=>'pr', 'SA'=>'sa', 'RO'=>'ro', 'DK'=>'dk');
-                $this->wgrList['class'] = $class;
-
-                $skills = array('dr'=>explode("\n", PORTAL_WGR_DR_S),'wl'=>explode("\n", PORTAL_WGR_WL_S),'hu'=>explode("\n", PORTAL_WGR_HU_S),'wa'=>explode("\n", PORTAL_WGR_WA_S),'ma'=>explode("\n", PORTAL_WGR_MA_S),'pa'=>explode("\n", PORTAL_WGR_PA_S),'pr'=>explode("\n", PORTAL_WGR_PR_S),'sa'=>explode("\n", PORTAL_WGR_SA_S),'ro'=>explode("\n", PORTAL_WGR_RO_S),'dk'=>explode("\n", PORTAL_WGR_DK_S));
-                foreach ($skills as $k => $v) {
-                  $this->wgrList[$k] = $v;
-                }
-                            
-                $onoff = array('dr'=>PORTAL_WGR_DR,'wl'=>PORTAL_WGR_WL,'hu'=>PORTAL_WGR_HU,'wa'=>PORTAL_WGR_WA,'ma'=>PORTAL_WGR_MA,'pa'=>PORTAL_WGR_PA,'pr'=>PORTAL_WGR_PR,'sa'=>PORTAL_WGR_SA,'ro'=>PORTAL_WGR_RO,'dk'=>PORTAL_WGR_DK);
-                $this->wgrList['onoff'] = $onoff;
-                foreach ($onoff as $k => $v) {
-                  $this->wgrList[$k."_o"] = $v;
-                }
-
+                $data['class'] = array('dr'=>'dr', 'wl'=>'wl', 'hu'=>'hu', 'wa'=>'wa', 'ma'=>'ma', 'pa'=>'pa', 'pr'=>'pr', 'sa'=>'sa', 'ro'=>'ro', 'dk'=>'dk');
+                $data['skill'] = array('dr'=>explode("\n", PORTAL_WGR_DR_S),'wl'=>explode("\n", PORTAL_WGR_WL_S),'hu'=>explode("\n", PORTAL_WGR_HU_S),'wa'=>explode("\n", PORTAL_WGR_WA_S),'ma'=>explode("\n", PORTAL_WGR_MA_S),'pa'=>explode("\n", PORTAL_WGR_PA_S),'pr'=>explode("\n", PORTAL_WGR_PR_S),'sa'=>explode("\n", PORTAL_WGR_SA_S),'ro'=>explode("\n", PORTAL_WGR_RO_S),'dk'=>explode("\n", PORTAL_WGR_DK_S));
+                $data['onoff'] = array('dr'=>PORTAL_WGR_DR,'wl'=>PORTAL_WGR_WL,'hu'=>PORTAL_WGR_HU,'wa'=>PORTAL_WGR_WA,'ma'=>PORTAL_WGR_MA,'pa'=>PORTAL_WGR_PA,'pr'=>PORTAL_WGR_PR,'sa'=>PORTAL_WGR_SA,'ro'=>PORTAL_WGR_RO,'dk'=>PORTAL_WGR_DK);
+                $data['color'] = array('dr'=>'#FF7D0A', 'wl'=>'#9482C9', 'hu'=>'#ABD473', 'wa'=>'#C79C6E', 'ma'=>'#69CCF0', 'pa'=>'#F58CBA', 'pr'=>'#FFFFFF', 'sa'=>'#2459FF', 'ro'=>'#FFF569', 'dk'=>'#C41F3B');
+                $this->wgrList['data'] = $data;
         }
 
         protected function getBoxStatus($data) {
