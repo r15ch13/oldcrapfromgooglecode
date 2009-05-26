d48dcb258f3b9e9382a58eac2396aece1489840c
r15ch13@gmx.de <r15ch13@gmx.de@ed87435c-ff07-11dd-9d1a-b59b2e1864b6>
2009-05-26 22:20:39 +0000

    added wolframalpha bbcode

---
+++ de.r15ch13.wcf.data.message.bbcode.wolframalpha/files/lib/data/message/bbcode/WolframAlphaBBCode.class.php
@@ -0,0 +1,28 @@
+<?php
+require_once (WCF_DIR . 'lib/data/message/bbcode/BBCodeParser.class.php');
+require_once (WCF_DIR . 'lib/data/message/bbcode/BBCode.class.php');
+
+/**
+ * Parses the [wolframalpha] bbcode tag.
+ *
+ * @author	r15ch13
+ * @package	de.r15ch13.wcf.data.message.bbcode.wolframalpha
+ */
+class WolframAlphaBBCode implements BBCode {
+	/**
+	 * @see BBCode::getParsedTag()
+	 */
+	public function getParsedTag($openingTag, $content, $closingTag, BBCodeParser $parser) {
+		if ($parser->getOutputType() == 'text/html') {
+			// show template
+			WCF::getTPL()->assign(array(
+					'content'=>intval($content)
+			));
+			return WCF::getTPL()->fetch('WolframAlphaBBCodeTag');
+		}
+		else if ($parser->getOutputType() == 'text/plain') {
+			return $content;
+		}
+	}
+}
+?>
