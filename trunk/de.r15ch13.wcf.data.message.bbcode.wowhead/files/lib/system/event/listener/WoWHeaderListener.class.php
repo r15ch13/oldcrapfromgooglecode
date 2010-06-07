85ee6556630181a469f6fb9b2dd09c731e32169a
r15ch13@gmx.de <r15ch13@gmx.de@ed87435c-ff07-11dd-9d1a-b59b2e1864b6>
2010-06-07 20:07:38 +0000

    HTTP 301 Moved permanently

--- trunk/de.r15ch13.wcf.data.message.bbcode.wowhead/files/lib/system/event/listener/WoWHeaderListener.class.php
+++ trunk/de.r15ch13.wcf.data.message.bbcode.wowhead/files/lib/system/event/listener/WoWHeaderListener.class.php
@@ -7,7 +7,7 @@ class WoWHeaderListener implements EventListener {
      * @see EventListener::execute()
      */
     public function execute($eventObj, $className, $eventName) {
-           WCF::getTPL()->append('userMessages', "<script type=\"text/javascript\" src=\"http://www.wowhead.com/widgets/power.js\"></script>");
+           WCF::getTPL()->append('userMessages', "<script type=\"text/javascript\" src=\"http://static.wowhead.com/widgets/power.js\"></script>");
     }
 }
 ?>
